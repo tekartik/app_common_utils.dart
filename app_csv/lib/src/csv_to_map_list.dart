ad8282ac74e00f214ce914efac6fe160a93a966d
alex <alex@tekartik.com>
2021-03-19 17:10:46 +0100

    feat: add csv to map list

---
+++ app_csv/lib/src/csv_to_map_list.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:csv/csv.dart';
+
+/// Convert a csv (with an header row) to csv
+List<Map<String, dynamic>> csvToMapList(String csv,
+    {CsvToListConverter converter}) {
+  if (converter == null) {
+    /// Use the default eol
+    csv = LineSplitter.split(csv).join(defaultEol);
+    converter = const CsvToListConverter();
+  }
+  var rawList = converter.convert(csv);
+  if (rawList.isEmpty) {
+    throw UnsupportedError('csv cannot be empty');
+  }
+
+  var keys = rawList[0].cast<String>();
+  var data = rawList.sublist(1);
+
+  var list = <Map<String, Object>>[];
+
+  for (var row in data) {
+    var map = <String, Object>{
+      for (var i = 0; i < keys.length; i++) keys[i]: row[i]
+    };
+    list.add(map);
+  }
+  return list;
+}
---
+++ app_csv/lib/src/map_list_to_csv.dart
@@ -0,0 +1,42 @@
+import 'package:csv/csv.dart';
+
+/// Convert a map list to csv
+String mapListToCsv(List<Map<String, dynamic>> mapList,
+    {ListToCsvConverter converter, Object nullValue = ''}) {
+  converter ??= const ListToCsvConverter();
+  var data = <List>[];
+  var keys = <String>[];
+  var keyIndexMap = <String, int>{};
+
+  // Add the key and fix previous records
+  int _addKey(String key) {
+    var index = keys.length;
+    keyIndexMap[key] = index;
+    keys.add(key);
+    for (var dataRow in data) {
+      dataRow.add(null);
+    }
+    return index;
+  }
+
+  for (var map in mapList) {
+    // This list might grow if a new key is found
+    var dataRow = []..length = keyIndexMap.length;
+    // Fix missing key
+    map.forEach((key, value) {
+      value ??= nullValue;
+      var keyIndex = keyIndexMap[key];
+      if (keyIndex == null) {
+        // New key is found
+        // Add it and fix previous data
+        keyIndex = _addKey(key);
+        // grow our list
+        dataRow = List.from(dataRow, growable: true)..add(value);
+      } else {
+        dataRow[keyIndex] = value;
+      }
+    });
+    data.add(dataRow);
+  }
+  return converter.convert(<List>[keys, ...data]);
+}
