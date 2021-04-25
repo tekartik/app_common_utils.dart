424aa0c7e28cfc4a5c561865d76f0a66369b447f
alex <alex@tekartik.com>
2021-04-25 20:48:46 +0200

    fix: lint directive ordering

--- app_web_socket/lib/src/web_socket_io.dart
+++ app_web_socket/lib/src/web_socket_io.dart
@@ -1,5 +1,5 @@
-import 'package:tekartik_web_socket_io/web_socket_io.dart';
 import 'package:tekartik_web_socket/web_socket.dart';
+import 'package:tekartik_web_socket_io/web_socket_io.dart';
 
 WebSocketChannelClientFactory webSocketChannelClientFactory =
     webSocketChannelClientFactoryIo;
--- app_web_socket/lib/src/web_socket_web.dart
+++ app_web_socket/lib/src/web_socket_web.dart
@@ -1,5 +1,5 @@
-import 'package:tekartik_web_socket_browser/web_socket_browser.dart';
 import 'package:tekartik_web_socket/web_socket.dart';
+import 'package:tekartik_web_socket_browser/web_socket_browser.dart';
 
 WebSocketChannelClientFactory webSocketChannelClientFactory =
     webSocketClientChannelFactoryBrowser;
