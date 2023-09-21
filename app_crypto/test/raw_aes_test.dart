f83b991694729140f7e7bb390455a1c8a614528d
Alexandre Roux <alex@tekartik.com>
2023-09-21 17:05:09 +0200

    test: workaround encrypt breaking change

--- app_crypto/test/raw_aes_test.dart
+++ app_crypto/test/raw_aes_test.dart
@@ -1,16 +1,20 @@
+import 'dart:typed_data';
+
 import 'package:encrypt/encrypt.dart';
 import 'package:test/test.dart';
 
-String encrypt(String decoded, String password) {
+String aesEncrypt(String decoded, String password) {
   final key = Key.fromUtf8(password);
-  final iv = IV.fromLength(16);
+  // final iv = IV.fromLength(16);
+  final iv = IV(Uint8List(16));
   final encrypter = Encrypter(AES(key));
   return encrypter.encrypt(decoded, iv: iv).base64;
 }
 
-String decrypt(String encoded, String password) {
+String aesDecrypt(String encoded, String password) {
   final key = Key.fromUtf8(password);
-  final iv = IV.fromLength(16);
+  // final iv = IV.fromLength(16);
+  final iv = IV(Uint8List(16));
   final encrypter = Encrypter(AES(key));
   return encrypter.decrypt(Encrypted.fromBase64(encoded), iv: iv);
 }
@@ -18,6 +22,7 @@ String decrypt(String encoded, String password) {
 void main() {
   test('AES encrypt/decrypt', () {
     var password = r'E4x*$TwbkJC-xK4KGC4zJF9j*Rh&WLgR';
-    expect(decrypt('amGhyRRLUIoE59IiEys5Vw==', password), 'test');
+    expect(aesEncrypt('test', password), 'amGhyRRLUIoE59IiEys5Vw==');
+    expect(aesDecrypt('amGhyRRLUIoE59IiEys5Vw==', password), 'test');
   });
 }
