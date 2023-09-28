883f14b839cd367f4a5744f6ffe3bd879d28efed
Alexandre Roux <alex@tekartik.com>
2023-09-28 20:44:39 +0200

    api export aesEncrypt

--- app_crypto/lib/encrypt.dart
+++ app_crypto/lib/encrypt.dart
@@ -2,5 +2,9 @@ export 'package:tekartik_app_crypto/src/encrypt.dart'
     show encrypt, decrypt, StringEncrypter, defaultEncryptedFromRawPassword;
 
 export 'src/aes.dart'
-    show aesEncrypterFromPassword, encryptTextPassword16FromText;
+    show
+        aesEncrypterFromPassword,
+        encryptTextPassword16FromText,
+        aesDecrypt,
+        aesEncrypt;
 export 'src/salsa20.dart' show salsa20EncrypterFromPassword;
