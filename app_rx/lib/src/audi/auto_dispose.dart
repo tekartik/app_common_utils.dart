104f16a8d8010c397983e9dce1c05d35ce531b2d
Alexandre Roux <alex@tekartik.com>
2025-03-27 15:20:36 +0100

    app_rx: add audiAddBroadcastValueStream

--- app_rx/lib/src/audi/auto_dispose.dart
+++ app_rx/lib/src/audi/auto_dispose.dart
@@ -1,9 +1,16 @@
 import 'package:tekartik_app_rx/auto_dispose.dart';
+import 'package:tekartik_app_rx/helpers.dart';
 
 /// Auto dispose extension for rx
 extension AutoDisposeRxExtension on AutoDispose {
   /// Add a BehaviorSubject to the auto dispose list
   BehaviorSubject<T> audiAddBehaviorSubject<T>(BehaviorSubject<T> subject) {
+    return audiAdd(subject, subject.close);
+  }
+
+  /// Add a BehaviorSubject to the auto dispose list
+  BroadcastValueStream<T> audiAddBroadcastValueStream<T>(
+      BroadcastValueStream<T> subject) {
     return audiAdd(subject, subject.close);
   }
 }
