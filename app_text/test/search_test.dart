ad7c2d8687de6dff1b1c9dc0e9c1642bbe655371
Alexandre Roux <alex@tekartik.com>
2025-04-15 16:05:50 +0200

    add search find all

--- app_text/test/search_test.dart
+++ app_text/test/search_test.dart
@@ -2,7 +2,7 @@ import 'package:tekartik_app_text/search.dart';
 import 'package:test/test.dart';
 
 Future<void> main() async {
-  test('splitByWhitespace', () {
+  test('SearchTextFinder', () {
     var finder = SearchTextFinder(searchText: '  Ma éL  ');
     expect(finder.findIn(''), isFalse);
     expect(finder.findIn(' to ma'), isFalse);
@@ -10,5 +10,9 @@ Future<void> main() async {
     expect(finder.findIn('Elev   ma'), isTrue);
     expect(finder.findAllIn('Elev   ma'), isFalse);
     expect(finder.findAllIn('ma Elev   mca'), isTrue);
+
+    finder = SearchTextFinder(searchText: 'i l');
+    expect(finder.findAllIn('I loon'), isTrue);
+    expect(finder.findAllIn('Delice'), isFalse);
   });
 }
