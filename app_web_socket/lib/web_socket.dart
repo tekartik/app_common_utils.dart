0e8e4ba72b749618634ec5a85030a531ee42c583
Alexandre Roux <alex@tekartik.com>
2025-07-14 17:39:27 +0200

    feat: export web socket

--- app_web_socket/lib/web_socket.dart
+++ app_web_socket/lib/web_socket.dart
@@ -2,6 +2,8 @@ import 'package:tekartik_web_socket/web_socket.dart';
 
 import 'src/web_socket.dart' as src;
 
+export 'package:tekartik_web_socket/web_socket.dart';
+
 /// The Web socket client factory for your flutter application.
 ///
 WebSocketChannelClientFactory get webSocketChannelClientFactory =>
