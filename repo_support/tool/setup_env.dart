51d37fda8cf7eb33ea7c705d307508d97f7e3dd2
Alexandre Roux <alex@tekartik.com>
2025-12-17 14:53:02 +0100

    fix ci

--- repo_support/tool/setup_env.dart
+++ repo_support/tool/setup_env.dart
@@ -1,5 +1,11 @@
+import 'package:dev_build/shell.dart';
 import 'package:tekartik_ci/setup_ci_github.dart';
 
 Future main() async {
-  await sudoSetupSqlite3Lib();
+  try {
+    await sudoSetupSqlite3Lib();
+  } catch (e) {
+    print('Could not setup sqlite3 lib: $e');
+  }
+  await run('dart pub global activate process_run');
 }
