1c4dec9fed94c773dc290fa1b5dffc5624cfdb77
Alexandre Roux <alex@tekartik.com>
2026-03-15 19:46:52 +0100

    fix public url

--- app_http/test/app_http_test.dart
+++ app_http/test/app_http_test.dart
@@ -1,17 +1,17 @@
 import 'package:tekartik_app_http/app_http.dart';
 import 'package:test/test.dart';
 
-var simleJsonUrl =
-    'https://firebasestorage.googleapis.com/v0/b/tekartik-free-dev.appspot.com/o/test%2Fexpected%2Ftest.json?alt=media';
+var simplePublicUrl = 'https://cdn.ampproject.org/v0.js';
+
 void main() {
   group('app_http', () {
     test('compat', () {
       expect(httpClientFactory, httpClientFactoryUniversal);
     });
     test('simple', () async {
-      // If this fails, it is because the file has been removed from firebase storage
+      // If this fails, update the url!
       var response = await httpClientFactoryUniversal.newClient().get(
-        Uri.parse(simleJsonUrl),
+        Uri.parse(simplePublicUrl),
       );
       print(response.body);
       expect(response.statusCode, httpStatusCodeOk);
