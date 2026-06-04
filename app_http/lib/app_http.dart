84a220e66a795bf35dc393c90d2caa37a1ee6580
Alexandre Roux <alex@tekartik.com>
2026-06-05 00:19:16 +0200

    feat: enhance HTTP exports with additional memory and server factories

--- app_http/lib/app_http.dart
+++ app_http/lib/app_http.dart
@@ -1,6 +1,9 @@
 export 'package:tekartik_http/http.dart';
+export 'package:tekartik_http/http_memory.dart'
+    show httpClientFactoryMemory, httpServerFactoryMemory, httpFactoryMemory;
 
-export 'src/app_http_io.dart' show httpClientFactoryIo;
+export 'src/app_http_io.dart'
+    show httpClientFactoryIo, httpFactoryIo, httpServerFactoryIo;
 export 'src/app_http_universal.dart'
     show httpClientFactory, httpClientFactoryUniversal;
 export 'src/app_http_web.dart' show httpClientFactoryBrowser;
--- app_http/lib/src/app_http_io.dart
+++ app_http/lib/src/app_http_io.dart
@@ -1,1 +1,3 @@
 export 'package:tekartik_http_io/http_client_io.dart' show httpClientFactoryIo;
+export 'package:tekartik_http_io/http_io.dart' show httpFactoryIo;
+export 'package:tekartik_http_io/http_server_io.dart' show httpServerFactoryIo;
